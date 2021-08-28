ac569281ea94c0449c4a76cd5b30d9e14ccc7762
Hectoralexa <87905201+Hectoralexan@users.noreply.github.com>
2021-08-28 11:02:41 -0600

    Update ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -2,10 +2,10 @@ precio = float(input('coloque el precio del producto: '))
 descuento = 0.1
 if precio >= 100 :
     print('aplica descuento')
-
+resultado = precio * descuento
 else:
     print('no aplica descuento')
+    resultado = 0
     
-    
-resultado = precio * descuento
+
 print('descuento de:',resultado)
