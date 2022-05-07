1d94af7b6a36a210537489047a59a31b68b3683d
Hectoralexa <87905201+Hectoralexan@users.noreply.github.com>
2022-05-07 12:03:12 -0600

    Update ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,11 +1,52 @@
-precio = float(input('coloque el precio del producto: '))
-descuento = 0.1
-if precio >= 100 :
-    print('aplica descuento')
-resultado = precio * descuento
-else:
-    print('no aplica descuento')
-    resultado = 0
-    
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-print('descuento de:',resultado)
+namespace EjemploEnFra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BarDbEntities db = new BarDbEntities())
+            {
+                //InsertarProductos
+                /*Producto oProducto = new Producto();
+                Console.WriteLine("Nombre");
+                oProducto.nomProd = Console.ReadLine();
+                Console.WriteLine("descripción");
+                oProducto.descripcion = Console.ReadLine();
+                Console.WriteLine("precio");
+                oProducto.precio = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("cantidad");
+                oProducto.cantidad = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("estado");
+                oProducto.estado = Convert.ToInt32(Console.ReadLine());
+
+                db.Producto.Add(oProducto);
+                db.SaveChanges();*/
+
+                //ActualizarProductos
+                /*Producto oProducto = db.Producto.Find(1);
+                oProducto.nomProd = "Cubatass";
+                db.Entry(oProducto).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();*/
+
+                //BorrarProductos
+                /*Producto oProductos = db.Producto.Find(9);
+                db.Producto.Remove(oProducto);
+                db.SaveChanges();*/
+
+                var lst = db.Producto;
+                foreach (var oProductos in lst)
+                {
+                    Console.WriteLine(oProductos.idProducto);
+                }
+                Console.WriteLine("Presione cualquier tecla para continuar");
+                Console.Read();
+            }
+        }
+    }
+}
